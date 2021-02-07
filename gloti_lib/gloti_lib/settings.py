3a8eea39f188c3eb6c07299db0a77f8b191e81f0
teefats <tee_fats@yahoo.com>
2021-02-07 18:32:46 +0000

    Added skeleton

--- gloti_lib/gloti_lib/settings.py
+++ gloti_lib/gloti_lib/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'locallibrary'
+    'locallibrary',
+    'catalog'
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
