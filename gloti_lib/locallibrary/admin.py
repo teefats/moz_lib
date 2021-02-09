eb3fe897988a469bc7958fc53638b4b7a3c04749
teefats <tee_fats@yahoo.com>
2021-02-09 22:16:49 +0000

    Added more labels

--- gloti_lib/locallibrary/admin.py
+++ gloti_lib/locallibrary/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Genre, Book, BookInstance,Author
 # Register your models here.
 admin.site.register(Genre)
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(BookInstance)
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    inlines = [BooksInstanceInline]
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
