7c7a9b57e61daf19be76a678a0821773fa728f54
teefats <tee_fats@yahoo.com>
2021-02-18 22:17:58 +0000

    Added book-detail patterns

--- gloti_lib/catalog/views.py
+++ gloti_lib/catalog/views.py
@@ -30,3 +30,12 @@ from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
+
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='war')[:5]
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context["some_data"] = 'This is simply some data' 
+        return context
+    
